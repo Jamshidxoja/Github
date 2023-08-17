338f806f41703579b5b75e75ea4866f7f0c727f0
Jamshid <xojaaliy@gmail.com>
2023-08-17 15:09:27 +0500

    second

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ddf");
         System.out.println("fddfdf");
+        System.out.println("DSds");
 
     }
 }
